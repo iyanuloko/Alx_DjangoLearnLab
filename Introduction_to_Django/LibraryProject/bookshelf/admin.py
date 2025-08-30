b582cd21dce1335d1f2100428ffba5b668699a3f
iyanuloko <iyanuoluwaloko@gmail.com>
2025-08-30 20:43:00 +0100

    Hopefully this works 9

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date')
+    list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
-    list_filter = ('published_date',)
+    list_filter = ('publication_year',)
 admin.site.register(Book)
 # Register your models here.
