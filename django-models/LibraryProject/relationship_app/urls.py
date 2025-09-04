7ac97ec9db2e9d6a48dbf148332252c6ca016a55
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-04 21:48:52 +0100

    Third project edit 1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView, SignUpView
 urlpatterns = [
     path('book_list/', views.list_books, name='book_list'),
     path('books_in_library/', views.LibraryDetailView.as_view(), name='books_in_library'),
+    path('register/', SignUpView.as_view(), name='register'),
+    path('login/', LoginView.as_view(template_name='templates/relationship_app/login.html'), name='login'),\
+    path('logout/', LogoutView.as_view(template_name='templates/relationship_app/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Library, Book
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
@@ -9,5 +12,9 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'templates/relationship_app/register.html'
 
 # Create your views here.
