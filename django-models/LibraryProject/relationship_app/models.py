b7e0e45114e814711e58ad30b4ea105da7b0fb01
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-04 19:08:10 +0100

    First project edit 2

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 class Author(models.Model):
     name = models.CharField()
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField()
@@ -9,9 +11,14 @@ class Book(models.Model):
 class Library(models.Model):
     name = models.CharField()
     books = models.ManyToManyField(Book)
+    def __str__(self):
+        return self.name
 
 class Librarian(models.Model):
     name = models.CharField()
     library = models.ManyToManyField(Library)
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
