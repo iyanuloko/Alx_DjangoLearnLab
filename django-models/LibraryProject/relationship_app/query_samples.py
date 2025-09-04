4a651f4476cf5d64afc0ffb9c1862c86ce38cd92
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-04 20:39:00 +0100

    First project edit 9

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,5 @@
 # to query books by specific author:
-from relationship_app.models import Book, Librarian, Library
+from relationship_app.models import Author, Book, Librarian, Library
 for books in Book.objects.all():
     if books.author == "author":
         print(books.title)
@@ -7,8 +7,8 @@ for books in Book.objects.all():
 library_books = Library.objects.get(name=library_name)
 print(library_books.books.all())
 
-author_books = Book.objects.get(name=author)
-print(author_books.title)
+author = Author.objects.get(name=author_name)
+books = Book.objects.filter(author=author)
 
-Librarian.objects.get(library=library_name)
-print(library_books.name)
+librarians = Librarian.objects.get(library=library_name)
+print(librarians.name)
