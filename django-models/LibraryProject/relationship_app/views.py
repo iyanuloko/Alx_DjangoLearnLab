ec301b6f1540010fd72f180222b3eb13c9c0dc35
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-05 18:46:49 +0100

    Fourth exercise edit 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -26,8 +26,9 @@ def register(request):
             form.save()
 
 def is_admin(user):
-    return user.role == 'admin'
-@user_passes_test(is_admin())
+    return user.is_authenticated and user.role == 'admin'
+
+@user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
