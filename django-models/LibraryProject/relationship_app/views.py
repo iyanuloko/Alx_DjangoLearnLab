35e497f99b18adcf9f1b5f9645fcf5e2350811e9
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-05 21:36:20 +0100

    Fourth exercise edit 8

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -25,22 +25,18 @@ def register(request):
         if form.is_valid():
             form.save()
 
-def admin_view(request):
-    if request.user.role == 'admin':
-        return render(request, 'relationship_app/admin_view.html')
-
 def is_admin(user):
     return user.is_authenticated and hasattr(user, "profile") and user.profile.role == "Admin"
 
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin(request):
     return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(lambda u: u.is_Librarian)
-def librarian_view(request):
+def Librarian(request):
     return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(lambda u: u.is_Member)
-def member_view(request):
+def Member(request):
     return render(request, 'relationship_app/member_view.html')
 # Create your views here.
