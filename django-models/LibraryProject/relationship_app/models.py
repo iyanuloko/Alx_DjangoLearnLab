be8a3553b6e8b7d75fefa55493cef09b5b89afe3
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-07 20:56:06 +0100

    Django advanced troubleshoot 9

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class Author(models.Model):
     name = models.CharField()
@@ -30,16 +28,14 @@ class Librarian(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
-
+    ROLE_CHOICES = (
+        ("Admin", "Admin"),
+        ("Librarian", "Librarian"),
+        ("Member", "Member"),
+        )
+    role = models.CharField(choices=ROLE_CHOICES, max_length=20)
 
     def __str__(self):
         return f"{self.user.username} ({self.role})"
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-    else:
-        instance.userprofile.save()
 # Create your models here.
