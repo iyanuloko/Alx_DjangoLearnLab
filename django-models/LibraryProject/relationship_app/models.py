fa5c92b3e553ba9cddb7ea1c199116f8fbf123f8
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-07 21:13:34 +0100

    Django advanced troubleshoot 10

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Author(models.Model):
     name = models.CharField()
@@ -38,4 +40,10 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username} ({self.role})"
 
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        instance.userprofile.save()
 # Create your models here.
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -30,7 +30,7 @@ def is_admin(user):
     return user.is_authenticated and  hasattr(user, "userprofile") and user.userprofile.role == "Admin"
 
 @user_passes_test(is_admin)
-def Admin(request):
+def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(lambda u: u.is_Librarian)
