b82d1e59f4e9ff610ce15e2a1e1dd0d6898fc1cc
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-08 17:30:40 +0100

    This exercise is draining me 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -27,13 +27,13 @@ def register(request):
             form.save()
 
 def is_admin(user):
-    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 def is_librarian(user):
-    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
 
 def is_member(user):
-    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
 @login_required
 @user_passes_test(is_admin)
