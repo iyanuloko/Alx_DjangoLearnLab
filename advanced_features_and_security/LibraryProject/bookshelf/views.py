1a8dec343cd5fb852b904d1599999f5c1757b60e
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-14 23:40:47 +0100

    Second exercise edit 5

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ExampleForm
+
 @permission_required("bookshelf.can_view")
 def view_book(request):
     return render(request, 'relationship_app/book_list.html')
@@ -15,4 +17,15 @@ def add_book(request):
 def delete_book(request):
     return render(request, 'relationship_app/delete_book_view.html')
 
+def contact_view(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # handle the data — e.g., save to DB, send email, etc.
+            # name = form.cleaned_data['name']
+            # email = form.cleaned_data['email']
+            # message = form.cleaned_data['message']
+            return redirect('bookshelf/form_example')  # change target as needed
+    else:
+        form = ContactForm()
 # Create your views here.
