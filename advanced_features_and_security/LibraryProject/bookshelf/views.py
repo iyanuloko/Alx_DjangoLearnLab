1f6dec3a4e0cdd494739156d1b6ce43951a65275
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-14 23:06:56 +0100

    Second exercise edit 2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 @permission_required("bookshelf.can_view")
 def view_book(request):
-    return render(request, 'relationship_app/add_book_view.html')
+    return render(request, 'relationship_app/book_list.html')
 
-@permission_required("bookshelf.can_edit")
+@permission_required("bookshelf.can_edit", raise_exception=True)
 def change_book(request):
     return render(request, 'relationship_app/change_book_view.html')
 
