e1f29adb0f463465bf61ab36b9d87d6c9628a35a
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-14 07:04:25 +0100

    First exercise edit 2

--- django-models/LibraryProject/bookshelf/admin.py
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+admin.site.register(CustomUser, UserAdmin)
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
