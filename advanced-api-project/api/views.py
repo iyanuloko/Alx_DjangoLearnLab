649974f9e5433dc94b6f07efea1fe80a62ab08cb
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-26 13:05:52 +0100

    Second exercise adv. api edit 6

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,27 +1,20 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.viewsets import ReadOnlyModelViewSet
-
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 
 class ListView(generics.ListAPIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class ListAPIView(ReadOnlyModelViewSet):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [AllowAny]
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
 
 class DetailView(generics.RetrieveAPIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
