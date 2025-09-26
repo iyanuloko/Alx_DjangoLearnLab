8c74701fff290f273b02ff9078c996daa0e47e93
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-26 13:28:22 +0100

    Third exercise adv. api edit 1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
@@ -10,7 +10,16 @@ class ListView(generics.ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ('title', 'author')
+    ordering_fields = ('title', 'publication_year')
 
+    def get_queryset(self):
+        queryset = Book.objects.all()
+        title = self.request.GET.get('title')
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        return queryset
 
 class DetailView(generics.RetrieveAPIView):
     authentication_classes = [TokenAuthentication]
@@ -35,5 +44,3 @@ class DeleteView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
-# Create your views here.
