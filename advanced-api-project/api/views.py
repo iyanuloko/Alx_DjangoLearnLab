3ef68ee43eb69f109a17f7bc1d976c9e14957b1b
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-26 13:10:22 +0100

    Second exercise adv. api edit 8

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
