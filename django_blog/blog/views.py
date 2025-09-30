9d74027d4ce5252a53a106a9cbcf8f9d8933f32d
iyanuloko <iyanuoluwaloko@gmail.com>
2025-09-30 15:10:00 +0100

    Second exercise edit 1

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import SignUpForm
 
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = 'blog/register.html'
+
+class ProfileView(CreateView):
+    
 # Create your views here.
