8f38d270c8712294f1925b29ed05a1c3e48ca09a
iyanuloko <iyanuoluwaloko@gmail.com>
2025-10-03 16:01:42 +0100

    Search and tag integration edit 2

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag)
+    tags = TaggableManager()
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
