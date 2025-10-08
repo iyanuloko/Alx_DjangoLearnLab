9be35cce786cd321cf3b4355ecbc0cb8cd98f439
iyanuloko <iyanuoluwaloko@gmail.com>
2025-10-08 15:45:48 +0100

    First exercise submission edit 4

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
 class SignUpFormSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
     class Meta:
         model = User
@@ -13,8 +15,10 @@ class SignUpFormSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError('Password must be at least 8 characters')
 
         def create(self, validated_data):
-            return User.objects.create_user(**validated_data)
-        
+            return get_user_model().objects.create_user(**validated_data)
+            Token.objects.create(user=self)
+            return user
+
 class ProfileViewSerializer(serializers.Serializer):
     class Meta:
         model = User
